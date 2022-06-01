356f364d70719738472fd8c6c48afe50d5a0f6ee
Vivek kumar <viveksinghssm496@gmail.com>
2022-06-01 19:39:43 +0530

    Added to main

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\tWelcome to User Registration Problem");
+        }
+    }
+}
